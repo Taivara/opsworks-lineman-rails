90dcffe5ac2c8c0281b01e380aed410bc7130eec
Roy Slagle <rslagle@taivara.com>
2014-12-10 13:57:47 -0500

    trying with git now

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,4 @@
-default['working_dir'] = '/srv/www/tmp'
+default['working_dir'] = '/srv/www/wiin_web'
 default['deploy_dir'] = '/srv/www/wiin_web'
+default['deploy_repo'] = 'git@github.com:Taivara/WiiN_Web.git'
+default['deploy_branch'] = 'master'
--- recipes/default.rb
+++ recipes/default.rb
@@ -14,7 +14,7 @@ node[:deploy].each do |application, deploy|
   ruby_block "write_key" do
     block do
       f = ::File.open("#{node['working_dir']}/id_deploy", "w")
-      f.print('-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAztrnd4/31X1CTBkHZSWEozTl6yRmXcIWh02xpLxyQ2MUqkMF\nJOY8fzB0BZgns/9gG2vrFSSRDvfKOf5ddhUpzj9qWo70ZzECQzeMnFBP6wh+UgN+\nFk6lGS+aIGFB6b9VEA1gyCPEuas2hjAYaoMKTodnqVSG0sW7pO/StRq8pRqdmxE/\nHUshSzoAbr/2ukIwSfHlaRKGC/wa8iTBsQAyV6gvW73oOfqsKQ/lOCD2OwanWyqf\niLF9IECRWOvOLvD8Sqo0Augvh+aiDYpnJBfM9gvBC8k07e6AzxERfsYi2XQ6grPE\nRMVYg88g9ZEXIQtWM7ewpzaKV0uFtjVSuC9ACQIDAQABAoIBAHDQFfiw/brjC2sx\nI0VYhtPzn4j0Wxe7KEo2ZzGuzKpPuQ96aa3MGyZKqJv8dXrHRgTugFERlkMRhKrI\nPPSPajNfhoNI/6Xfppo+C2OcsVk/UpUr7nIABi6MNYR/RlFS1fNhOG0uhLW12JCq\nXkaInPBjDHfe74C38jRrzwCSSlvIAzcniTjfo4L41Ep0pd6ukwvR8F9uNFoDulmW\nAD70IzlwfGs41M56ME1plg7Cx6IlWQcoHHD9n3GxM8Vc65oYzlyC0nxZWxkX4XD8\n5ZcPAYt4CZn/uGaO3kTnXEFUqMTqoUMauIlGmhGKxr+Z2DX4qjM4TIX6fI4pl36v\nHxjwhHkCgYEA5nklh2L8coPKsMzwKzKOF4e3nnCYWbFnJKJDsI49CehScSvryYDT\nUcqZjOHzRH63PNziOaSQHaVHCooYP0Mycn3gfC9z8IrEpHvvmth6Q/sgZNHzJUHj\nCiJu/aaCauk+lMN5V0JSt8hu0bLALH2cgNKx8iVKg//NdMVOM8LZKvMCgYEA5cQW\nX97NDdlS+1Iom2OSrb++DOqXoHSYZcO41FzgKiDWLkuhePXtVOZVqNNfOhZoHjWt\nFysCae2ZCbu58+sYxHcaM9P/45QYgOv2sCIKUF1kQS4m5MgGyaj39hKeF+6yB3V7\n3U1m19piLyCpS5ctzByTFvKFzG+ZpN9DtEfOsBMCgYEAzfVZKkG1I4jO62xY++Fz\nxWdNGdO48f5tE/nFz9NsjJwGgIglb68mKSOKULHiuUUb7tHdgHGlMDjw4WRDAtXE\nETiYEaI+U6lOzUbL+m6V5IZYjoBobtUQVZAr65SPW4cQm9SLbQUh8oa1McQxpBCk\neSLQTnowuwEv07g2iF8Jvy0CgYEAwTsjxgTOVrNKgmoaFHStSspSvvIcCqmD8TGl\nI5PoQgXTmqOobN4GdrwvlggNsgBeF+n0Y+Ob6BIV/oWOYYB6F+BCfQvxzCckZGgN\n0xsNqYCha9+wff9rFfWwc5Y8tsvblUJMpVUPBMF4iUHintvJfDsbyNS1ibThdjL9\n8YPiY1kCgYBxOsu4U5RwsLt4FeSM2Pe0+Z2RvNONPDxnGQsjvBWywV8J9oBgjnPY\nVLykmLUV27pCnM6cwM2Sgy8QkaEmQ6lhJjTmriR0nHGd6lQXKDeSUIudUcDnVOAU\n3NHaKL2W/aCVYO78jWl8bQ0PpMR7iVmEQMWp9vhSBlskRghaZMCZMg==\n-----END RSA PRIVATE KEY-----\n')
+      f.print('-----BEGIN RSA PRIVATE KEY-----\nMIIEpAIBAAKCAQEAsHvfywq2YUQjfRyphRH280yLeR3ZNqhZsrm4Nl7+QxVmp4wn\nVc9IH2A9mm9azTPj2N9CUP+F6qWxrIzIJqLZhDvtWo+fpzX5Bb2d+vZW8b1ybsIj\n9d6d4D426hg9E4AwfCh/lFAYLqMJkUzD2RySQCNbieE19veHZmHlq7HGvlj5HbjK\n/F6RTIT4Xp615Q0QN/d9eH0oUyTIyovIXSyrKdGiTwCF/VMTpzn+4H3iUDKtUZ85\n6MRMRwxekbghoUbJqZjDSrUbzesVe/COxlm5vvBTBnChyDclw+xb07VBqPl8Ctx2\nKQcTParuRmU7YFsMSF+6qaciAW63QRBi3+kViwIDAQABAoIBAQCB2IINRzJsWc+q\ni14F8/O3igfL8rQPzMlB//aSuk1kWeiPOUTvk3RC8XIhn/A0rgrjU0/dfEKxI3uX\nsnTISGS1L8owKR+TzNgM6zfCn1/UuEfrSZdfnIyXoe6RSYgWQuhpUPqcylkgZv/x\nEYO+x5Cq89izlyk3LWNHZpuuOTw6as7x/nC6zGSjUqEYAszCDxjCqAkIpbJMkRbe\n6q9sXdncoQjOxJIrQ9VFANEC+pkvVqC+ZabPISZ4kA5CciyFTB3d111vRolV4jGF\ntHz7FAZMOBmDvDmoqLJEwbB7tqDNZI1NhDtpncJJ/HX4HRW7Gk8ySaPZKmHn8lAu\nq65QeYohAoGBANmgVyPDPzpQ7nnh2hYQJc2quI5O+7KwMsLzbqgOnuFiGvb09VpE\nljgJr9kFQeVsXYbg5DEof2GClpG164yBpMwrhw7mcHj6OlAu5XPfHLAoTavf2CSe\netdKicHloSchmmn8ifd2pclf5dD3tTpUSPbkU0pgCBT8btfzxRwEH+LTAoGBAM+a\nXMk/qEynDDfyF39IFRW2HKuPvy3zM+yBMROR234kdfwbJK6S1HL/gUKaAd9L9UKI\nA8ZUHCetI6bPX47O81I/w6RHtGRTFRuF8pKmVUsU6GK59bNob4JQ+LqVuimmO2ON\nV66xZZxaCJsEG82nJGNelhWyh7+RHomjQv06vJ9pAoGAWw8qJv6lUCNZBYqpFB5B\nkyLiAjmuO4BaXRQ6svsbI7dHDRpGJzUXne7/s1Dt169WGv+yu5b0Zbqcge+a1bnR\nWNTFuEhXu2cIS1C0FZ2/hT338fjBmeDuCXNY3NbWbWc0EzDmEbOFfxMvOBbNr9Bg\nj13ONAqmlxFg199aFHTtWp0CgYEAg5peIXGKNfVbaIe+X4CJZTcJ27QHRJC8pLuN\nKdO3qbJhXMy0JzqLFM9l2Juafjw/oMpqYiplor0+MXuaHwSP+N7VHeoar9J/OfBi\nwpZQ0YTSf+tUl0OoFJtR4a4S0l4/na83G1X3jPMCK4qiOW8wQRHv4JdJOKY9DpGB\nYNgARKkCgYAwcHAxXVXtFsCXiiXc2E+b6goiP4V3abjuPigCvuSpNI1AULhFDBzS\nZTJp8+5fLuQdNv9eUPm1T1VrFtQFgu3H5rgi5JdBvk0GhhSCsx1qkCpX8x2HJxWc\ndBl9NSR4M9ol+WPuWMEM/n5x5sUeHh1ypi4HevY+C2usu3sdHn/zwg==\n-----END RSA PRIVATE KEY-----\n')
       f.close
     end
     not_if do ::File.exists?("#{node['working_dir']}/id_deploy"); end
@@ -30,24 +30,20 @@ node[:deploy].each do |application, deploy|
     variables("deploy_dir" => node['deploy_dir'])
   end
 
-  # deploy_revision node['hello_app']['deploy_dir'] do
-  #   scm_provider Chef::Provider::Git 
-  #   repo node['hello_app']['deploy_repo']
-  #   revision node['hello_app']['deploy_branch']
-  #   if secrets["deploy_key"]
-  #     git_ssh_wrapper "#{node['hello_app']['deploy_dir']}/git-ssh-wrapper" # For private Git repos 
-  #   end
-  #   enable_submodules true
-  #   shallow_clone false
-  #   symlink_before_migrate({}) # Symlinks to add before running db migrations
-  #   purge_before_symlink [] # Directories to delete before adding symlinks
-  #   create_dirs_before_symlink ["config"] # Directories to create before adding symlinks
-  #   symlinks({"config/local.config.php" => "config/local.config.php"})
-  #   # migrate true
-  #   # migration_command "php app/console doctrine:migrations:migrate" 
-  #   action :deploy
-  #   restart_command do
-  #     service "apache2" do action :restart; end
-  #   end
-  # end
+  deploy_revision node['working_dir'] do
+    scm_provider Chef::Provider::Git 
+    repo node['deploy_repo']
+    revision node['deploy_branch']
+    git_ssh_wrapper "#{node['working_dir']}/git-ssh-wrapper" # For private Git repos 
+    enable_submodules false
+    shallow_clone false
+    symlink_before_migrate({}) # Symlinks to add before running db migrations
+    purge_before_symlink [] # Directories to delete before adding symlinks
+    create_dirs_before_symlink ["config"] # Directories to create before adding symlinks
+    symlinks({"config/local.config.php" => "config/local.config.php"})
+    action :deploy
+    restart_command do
+      service "apache2" do action :restart; end
+    end
+  end
 end
